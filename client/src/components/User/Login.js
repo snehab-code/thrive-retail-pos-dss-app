a96806a320db395edf814086ff9b8ac16d3b6639
Sneha Bhat <nehabphn@gmail.com>
2020-02-27 00:49:40 +0530

    login/registration forms

--- client/src/components/User/Login.js
+++ client/src/components/User/Login.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-function Login() {
-    return (
-      <>
-        <div className="loginPanel">
-            <h3>LOGIN</h3>
-        </div>
-      </>
-    )
+class Login extends React.Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('handle submit')
+    }
+
+    render(){
+        return (
+            <>
+              <div className="loginPanel">
+                  
+                <form onSubmit={this.handleSubmit}>
+                  <h3>LOGIN</h3>
+                  <TextField size="small" label="username/email"/>
+                  <TextField size="small" type="password" label="password"/>
+                  <Button 
+                    size="small" 
+                    type="submit" 
+                  >
+                    Sign in
+                  </Button>
+                </form>
+                  <div>
+                    <Link to="/forgot-password">Forgot Password</Link>
+                  </div>
+              </div>
+              
+            </>
+        )
+    }
 }
 
 export default Login
--- client/src/components/User/Register.js
+++ client/src/components/User/Register.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-function Register() {
-    return (
-        <div>
-            Register
-        </div>
-    )
+class Register extends React.Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('handle submit')
+    }
+
+    render(){
+        return (
+            <>
+              <div className="loginPanel">
+                  
+                <form onSubmit={this.handleSubmit}>
+                  <h3>Register</h3>
+                  <TextField size="small" label="username"/>
+                  <TextField size="small" label="email"/>
+                  <TextField size="small" type="password" label="password"/>
+                  <Button 
+                    size="small" 
+                    type="submit" 
+                  >
+                    Sign up
+                  </Button>
+                </form>
+                  <div>
+                    <Link to="/forgot-password">Forgot Password</Link>
+                  </div>
+              </div>
+              
+            </>
+        )
+    }
 }
 
 export default Register
