6f2e0f9d8445f236a16db9f52de8f35e5f2330fb
Sneha Bhat <nehabphn@gmail.com>
2020-03-11 12:01:25 +0530

    added drawer

--- client/src/components/statics/Header.js
+++ client/src/components/statics/Header.js
@@ -3,15 +3,74 @@ import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import {startPostUserLogout} from '../../actions/user'
 import MenuIcon from '@material-ui/icons/Menu'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
 
-function Header(props) {
-    return (
+class Header extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            drawerIsOpen: false,
+            activeBusiness: ''
+        }
+    }
+
+    toggleDrawer = (open) => {
+        console.log('i ran')
+        this.setState({drawerIsOpen: open, activeBusiness: this.props.location.pathname.slice(12, 36)})
+    }
+
+    render() {
+        console.log(this.state)
+        return (
         <div className="header">
-            {props.location.pathname.includes('/businesses/') ?
+            
+            {this.props.location.pathname.includes('/businesses/') ?
+             <>
+             <Drawer open={this.state.drawerIsOpen} onClose={() => this.toggleDrawer(false)}>
+                <List>
+                    
+                    <ListItem button>
+                        <ListItemIcon><AccountBalanceWalletIcon/></ListItemIcon>
+                        <ListItemText primary={'expenses'} />
+                    </ListItem>
+                    
+                </List>
+                <Divider />
+                <List>
+                    <ListItem button>
+                        <ListItemIcon><AccountBalanceWalletIcon/></ListItemIcon>
+                        <ListItemText primary={'exp'} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon><AccountBalanceWalletIcon/></ListItemIcon>
+                        <ListItemText primary={'exp'} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon><AccountBalanceWalletIcon/></ListItemIcon>
+                        <ListItemText primary={'exp'} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon><AccountBalanceWalletIcon/></ListItemIcon>
+                        <ListItemText primary={'exp'} />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon><AccountBalanceWalletIcon/></ListItemIcon>
+                        <ListItemText primary={'exp'} />
+                    </ListItem>
+                </List>
+             </Drawer>
              <div className="logo">
-                <MenuIcon/>
+                <MenuIcon onClick={() => this.toggleDrawer(true)}/>
                 <Link to="/businesses"><h1>THRIVE</h1></Link>
              </div>
+             </>
              :
              <div className="logo">
                 <Link to="/"><h1>THRIVE</h1></Link>
@@ -20,11 +79,11 @@ function Header(props) {
             
             <div className="headerLinks">
                 {
-                    props.user.isLoggedIn ? 
+                    this.props.user.isLoggedIn ? 
                     <div className="headerLinks">
                     <Link to ="/businesses">Businesses</Link>
                     <Link to ="/user">Settings</Link>
-                    <Link to ="/" onClick={() => {props.dispatch(startPostUserLogout())}}>Logout</Link>
+                    <Link to ="/" onClick={() => {this.props.dispatch(startPostUserLogout())}}>Logout</Link>
                     </div>
                     :
                     <div className="headerLinks">
@@ -35,6 +94,7 @@ function Header(props) {
             </div>
         </div>
     )
+    }
 }
 
 const mapStateToProps = (state) => {
